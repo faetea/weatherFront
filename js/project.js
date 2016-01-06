0d5653ab73dea80d0132b4781703e5879621d8e2
Allison Sichling <saphron.scul@gmail.com>
2016-01-05 21:03:58 -0500

    creating modal windows for register/login

---
+++ js/project.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    });
+  });
+
+
